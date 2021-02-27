d64b0d5bba92fc5af29c573064ee5592d5c0a8de
kahverengi <ahmet.duruer@yandex.com>
2021-02-28 01:27:17 +0300

    Fix tooltip stucks on page change

--- js/tooltip.util.js
+++ js/tooltip.util.js
@@ -1,4 +1,10 @@
 import jQuery from "jquery";
+import { afterRouteEnter } from "routve";
+
+afterRouteEnter((context, next) => {
+  jQuery('[data-toggle="tooltip"], .tooltip').tooltip("hide");
+  next();
+});
 
 function setTooltip(element, [place, value]) {
   if (value !== jQuery(element).attr("data-original-title")) {
