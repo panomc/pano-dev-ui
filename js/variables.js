b463931be38831a8da7a6c0cd1637999fa40e110
kahverengi <ahmet.duruer@yandex.com>
2022-02-27 19:20:08 +0300

    Add ui, panel and setup url to variables

--- js/variables.js
+++ js/variables.js
@@ -1,4 +1,8 @@
 export const API_URL = import.meta.env.VITE_API_URL;
+export const UI_URL = import.meta.env.VITE_UI_URL;
+export const PANEL_URL = import.meta.env.VITE_PANEL_URL;
+export const SETUP_URL = import.meta.env.VITE_SETUP_URL;
+
 export const COOKIE_PREFIX = import.meta.env.VITE_COOKIE_PREFIX;
 
 export const CSRF_TOKEN_COOKIE_NAME = "CSRFToken"
