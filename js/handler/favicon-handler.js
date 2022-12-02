3f6f3571f54dc4409b97a9332241bc3caed1b362
kahverengi <ahmet@duruer.dev>
2022-12-02 22:45:24 +0300

    Reformat codes

--- js/handler/favicon-handler.js
+++ js/handler/favicon-handler.js
@@ -3,7 +3,9 @@ import { returnError } from "../api.proxy.js";
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function GET({ url, fetch }) {
-  let body = await api.GET({ path: url.pathname, parse: false }).catch(returnError);
+  let body = await api
+    .GET({ path: url.pathname, parse: false })
+    .catch(returnError);
 
   if (body.status === 404 || body.error === "NETWORK_ERROR") {
     return fetch("/assets/img/minecraft-icon.png");
